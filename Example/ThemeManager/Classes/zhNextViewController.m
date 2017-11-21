ca14cec0f99cd42cf1250120f8f437357c862551
张浩 <hao01.zhang@inin88.com>
2017-11-21 09:19:22 +0800

    update

--- Example/ThemeManager/Classes/zhNextViewController.m
+++ Example/ThemeManager/Classes/zhNextViewController.m
@@ -12,6 +12,7 @@
 @interface zhNextViewController ()
 
 @property (nonatomic, strong) zhTestView *testView;
+@property (nonatomic, strong) UISegmentedControl *segmentedControl;
 
 @end
 
@@ -22,6 +23,11 @@
     self.view.backgroundColor = [UIColor whiteColor];
     self.navigationController.navigationBar.zh_tintColorPicker = TMColorWithKey(@"color04");
 
+    [self setupTextView];
+    [self setupSegmentedControl];
+}
+
+- (void)setupTextView {
     _testView = [[zhTestView alloc] init];
     _testView.size = CGSizeMake(270, 300);
     _testView.center = CGPointMake(self.view.width / 2, self.view.height / 2);
@@ -29,10 +35,10 @@
     [_testView.aButton addTarget:self action:@selector(changeThemeClicked) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_testView];
     
-//    UIImage *img = [UIImage imageNamed:@"style3_me"];
-//    UIColor *labelColor = [UIColor magentaColor];
-//    UIColor *btntieColor = [UIColor orangeColor];
-//    [_testView setLabelBackgroundColor:labelColor buttonTitleColor:btntieColor andState:5 image:img imgSize:CGSizeMake(100, 100)];
+    //    UIImage *img = [UIImage imageNamed:@"style3_me"];
+    //    UIColor *labelColor = [UIColor magentaColor];
+    //    UIColor *btntieColor = [UIColor orangeColor];
+    //    [_testView setLabelBackgroundColor:labelColor buttonTitleColor:btntieColor andState:5 image:img imgSize:CGSizeMake(100, 100)];
     
     NSDictionary *dict1 = @{ThemeDay : [UIColor cyanColor], ThemeNight : [UIColor purpleColor]};
     NSDictionary *dict2 = @{ThemeDay : [UIColor cyanColor], ThemeNight : [UIColor purpleColor]};
@@ -46,6 +52,47 @@
      [NSNumber numberWithInteger:2],
      TMImageWithDict(dict3),
      [NSValue valueWithCGSize:CGSizeMake(70, 70)]];
+}
+
+- (void)setupSegmentedControl {
+    NSMutableArray *segmentedArray = [[NSMutableArray alloc] initWithObjects:@"体育", @"娱乐", @"新闻", nil];
+    
+    UIColor *mainColor = [UIColor colorWithHexString:@"A874E0"];
+    _segmentedControl = [[UISegmentedControl alloc] initWithItems:segmentedArray];
+    _segmentedControl.selectedSegmentIndex = 0;
+    _segmentedControl.clipsToBounds = YES;
+    
+    NSDictionary *dict1 = @{ThemeDay : [UIImage imageWithColor:mainColor],
+                            ThemeNight : [UIImage imageWithColor:[UIColor redColor]]};
+    
+    SEL sel = @selector(setDividerImage:forLeftSegmentState:rightSegmentState:barMetrics:);
+    [_segmentedControl zh_addThemePickerForSelector:sel
+                                      withArguments:
+     TMImageWithDict(dict1),
+     @(UIControlStateNormal),
+     @(UIControlStateNormal),
+     @(UIBarMetricsDefault)];
+    
+    NSDictionary *attributesSel = @{NSForegroundColorAttributeName:[UIColor whiteColor],
+                                    NSFontAttributeName:[UIFont systemFontOfSize:16]};
+    [_segmentedControl setTitleTextAttributes:attributesSel forState:UIControlStateSelected];
+    
+    NSDictionary *attributesNor = @{NSForegroundColorAttributeName:[UIColor darkGrayColor],
+                                    NSFontAttributeName:[UIFont systemFontOfSize:16]};
+    [_segmentedControl setTitleTextAttributes:attributesNor forState:UIControlStateNormal];
+    
+    NSDictionary *dict2 = @{ThemeDay : [UIColor colorWithHexString:@"C1A9E6"],
+                            ThemeNight : [UIColor colorWithHexString:@"FF4747"]};
+    _segmentedControl.zh_tintColorPicker = TMColorWithDict(dict2);
+    _segmentedControl.layer.zh_borderColorPicker = TMColorWithDict(dict2);
+    _segmentedControl.layer.borderWidth = 1;
+    [self.view addSubview:_segmentedControl];
+    
+    _segmentedControl.size = CGSizeMake(_testView.width, 40);
+    CGFloat centerY = (self.view.height - _testView.bottom) / 2 + _testView.bottom;
+    _segmentedControl.centerY = centerY;
+    _segmentedControl.centerX = self.view.width / 2;
+    _segmentedControl.layer.cornerRadius = _segmentedControl.height / 2;
 }
 
 - (void)changeThemeClicked {
